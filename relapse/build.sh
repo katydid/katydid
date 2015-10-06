552749656d2080da058c9074e3967715fd372106
Walter Schulze <awalterschulze@gmail.com>
2015-10-06 17:01:21 +0200

    split imports from grammar

--- relapse/build.sh
+++ relapse/build.sh
@@ -14,10 +14,11 @@
 
 echo "//  DO NOT EDIT" > all.bnf
 echo "//  This is generated file, see build.sh" >> all.bnf
-echo "//  Sources: ../expr/license.bnf ../expr/lexer.bnf, relapse.bnf, ../expr/expr.bnf, ../expr/keyword.bnf" >> all.bnf
+echo "//  Sources: ../expr/license.bnf ../expr/lexer.bnf, import.bnf, relapse.bnf, ../expr/expr.bnf, ../expr/keyword.bnf" >> all.bnf
 echo "" >> all.bnf
 cat ../expr/license.bnf >> all.bnf
 cat ../expr/lexer.bnf >> all.bnf
+cat import.bnf >> all.bnf
 cat relapse.bnf >> all.bnf
 cat ../expr/expr.bnf >> all.bnf
 cat ../expr/keyword.bnf >> all.bnf
