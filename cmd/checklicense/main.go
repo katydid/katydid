e8f2d8990e76b93259b159119da0deab379e96ff
Walter Schulze <awalterschulze@gmail.com>
2016-03-23 16:21:58 +0200

    some docs

--- cmd/checklicense/main.go
+++ cmd/checklicense/main.go
@@ -12,6 +12,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+//Internal command that checks each file for a license
 package main
 
 import (
