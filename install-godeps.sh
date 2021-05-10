63b59c780b080d42138fecbb32c7cf1c59e41777
Elphas Tori <elphastori@gmail.com>
2021-05-10 14:24:00 +0200

    fixed bug preventing rerunning build script (#10)

--- install-godeps.sh
+++ install-godeps.sh
@@ -2,7 +2,7 @@
 set -xe
 mkdir -p $GOPATH/src/github.com/gogo
 git clone https://github.com/gogo/protobuf $GOPATH/src/github.com/gogo/protobuf
-mkdir -p $GOPATH/src/githbub.com/goccmack
+mkdir -p $GOPATH/src/github.com/goccmack
 git clone https://github.com/goccmack/gocc $GOPATH/src/github.com/goccmack/gocc
 mkdir -p $GOPATH/src/github.com/awalterschulze
 git clone https://github.com/awalterschulze/goderive $GOPATH/src/github.com/awalterschulze/goderive
